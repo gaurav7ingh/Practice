76e3f0396cdc8c8a2126e549c62c49e3499e4ad4
gaurav7ingh <gauravsingh99626@gmail.com>
2022-11-17 19:09:58 +0530

    pattern printing

--- practice perpose/src/a4_pattern_two_opposite_triangle/Solution.java
+++ practice perpose/src/a4_pattern_two_opposite_triangle/Solution.java
@@ -3,12 +3,29 @@ package a4_pattern_two_opposite_triangle;
 public class Solution {
 
 	public static void printPattern(int N) {
-		for (int i = 0; i < N; i++) {
-			StringBuilder sb = new StringBuilder("");
-			for (int j = 0; j < i; j++) {
-				sb.append("*");
+		for(int i = 1; i < N; i++) {
+			for(int j = i; j < N; j++) {
+				System.out.print("  ");
 			}
-			System.out.println(sb.toString());
+			for(int j = 0; j < i; j++) {
+				System.out.print("* ");
+			}
+			for(int j = 1; j < i; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < i; j++) {
+				System.out.print("  ");
+			}
+			for(int j = i; j < N; j++) {
+				System.out.print("* ");
+			}
+			for(int j = i; j < N-1; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
 		}
 	}
 
