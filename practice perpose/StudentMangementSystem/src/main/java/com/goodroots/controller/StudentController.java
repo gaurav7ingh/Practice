2474590e6f07e754e30f629173c355c043724bfd
gaurav7ingh <gauravsingh99626@gmail.com>
2023-02-02 22:36:55 +0530

    security updated

--- practice perpose/StudentMangementSystem/src/main/java/com/goodroots/controller/StudentController.java
+++ practice perpose/StudentMangementSystem/src/main/java/com/goodroots/controller/StudentController.java
@@ -2,10 +2,12 @@ package com.goodroots.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.goodroots.LoginDTO;
 import com.goodroots.exception.StudentException;
 import com.goodroots.model.Student;
 import com.goodroots.service.StudentService;
@@ -19,6 +21,12 @@ public class StudentController {
 	public ResponseEntity<Student> updateStudentHandler(@RequestBody Student student) throws StudentException{
 		student = studentService.updateStudent("this", student);
 		return new ResponseEntity<Student>(student,HttpStatus.ACCEPTED);
+	}
+	
+	@GetMapping
+	public ResponseEntity<Boolean> loginHandler(@RequestBody LoginDTO loginCredential) throws StudentException{
+		boolean ans =  studentService.logIn(loginCredential.getUsername(), loginCredential.getPassword());
+		return new ResponseEntity<Boolean>(ans,HttpStatus.OK);
 	}
 	
 }
