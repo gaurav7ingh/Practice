aed8d090664ad87500484153d65a5859a4f07957
gaurav7ingh <gauravsingh99626@gmail.com>
2023-03-19 23:07:47 +0530

    maximum Sum Subarray

---
+++ practice perpose/src/a44_maximumSumSubarray/Solution.java
@@ -0,0 +1,29 @@
+package a44_maximumSumSubarray;
+
+public class Solution {
+
+	class Main {
+		// This function calculates the maximum sum of a subarray, for a given array.
+		public static int maximumSumSubarray(int[] arr) {
+			int max_so_far = Integer.MIN_VALUE;
+			int max_ending_here = 0;
+			for (int i = 0; i < arr.length; i++) {
+				max_ending_here += arr[i];
+				if (max_so_far < max_ending_here) {
+					max_so_far = max_ending_here;
+				}
+				if (max_ending_here < 0) {
+					max_ending_here = 0;
+				}
+			}
+			return max_so_far;
+		}
+
+		// Main Function
+		public static void main(String[] args) {
+			int[] arr = { 2, -3, 4, -1, -2, 1, 5, -3 };
+			int ans = maximumSumSubarray(arr);
+			System.out.println(ans);
+		}
+	}
+}
