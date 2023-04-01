8118b97b99a2ab9fdc2b31f32f7f3b139ed396cd
gaurav7ingh <gauravsingh99626@gmail.com>
2023-04-01 23:28:55 +0530

    duplicates removed

--- practice perpose/StudentMangementSystem/src/main/java/com/goodroots/repository/AdminRepo.java
+++
@@ -1,9 +0,0 @@
-package com.goodroots.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.goodroots.model.Admin;
-
-public interface AdminRepo extends JpaRepository<Admin, String> {
-
-}
--- practice perpose/StudentMangementSystem/src/main/java/com/goodroots/repository/StudentRepo.java
+++
@@ -1,9 +0,0 @@
-package com.goodroots.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.goodroots.model.Student;
-
-public interface StudentRepo extends JpaRepository<Student, String> {
-
-}
